899b2deb83463c841a65ac5b15b5107960a2c4bf
jahazieljbh <jahaziel.hernandez.jbh@gmail.com>
2023-08-09 18:46:25 -0600

    fix ENV

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: 'http://localhost:3000/api/v1/' });
+const API = axios.create({ baseURL: import.meta.env.VITE_BACKEND_URL });
 
 API.interceptors.request.use((req) => {
 	if (localStorage.getItem("profile")) {
